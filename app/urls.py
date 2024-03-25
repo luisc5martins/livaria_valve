abee74f8c92cc97ed1bdba2058da3aeaf0aee3fc
luisc5martins <luisc.martinsjunior5@gmail.com>
2024-03-25 11:48:00 -0300

    correções nos arquivos do Autor

--- app/urls.py
+++ app/urls.py
@@ -9,13 +9,14 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import CategoriaViewSet, EditoraViewSet, UserViewSet
+from core.views import CategoriaViewSet, EditoraViewSet, UserViewSet, AutorViewSet
 
 router = DefaultRouter()
 
 router.register(r"users", UserViewSet, basename="users")
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
+router.register(r"autores", AutorViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- core/models/categoria.py
+++ core/models/categoria.py
@@ -5,5 +5,5 @@ class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
 
-def __str__(self):
-    return f"{self.descricao} ({self.id})"
+    def __str__(self):
+        return f"{self.descricao} ({self.id})"
